1a13674f9ba328ba0a31a45cd4ad9de42d40b1b9
Secret <m0916041596@gmail.com>
2018-05-18 14:25:42 +0800

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,27 @@
+#define T 2
+void setup() {
+  // put your setup code here, to run once:
+  for(int i=2;i<7;i++)
+  {
+      pinMode(i,OUTPUT);
+      digitalWrite(i,HIGH);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i =2;i<7;i++)
+  {
+    digitalWrite(i,LOW);
+    delay(T);
+    digitalWrite(i,HIGH);
+    delay(T);  
+  }
+  for(int i =6;i>1;i--)
+  {
+    digitalWrite(i,LOW);
+    delay(T);
+    digitalWrite(i,HIGH);
+    delay(T);  
+  }
+}
